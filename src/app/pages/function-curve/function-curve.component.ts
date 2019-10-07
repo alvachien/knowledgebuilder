bf92f83e698cab03accfd373c390e4897bda6c68
Alva Chien <alvachien@163.com>
2019-10-07 17:43:23 +0800

    Try to add more curves

--- src/app/pages/function-curve/function-curve.component.ts
+++ src/app/pages/function-curve/function-curve.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { EChartOption } from 'echarts';
+import { EChartOption, EChartTitleOption } from 'echarts';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -28,6 +28,23 @@ export class FunctionCurveComponent implements OnInit {
       value: 'floor',
       label: 'y = Floor(x)',
       isLeaf: true,
+    }, {
+      value: 'trigono',
+      label: 'Trigonometric function',
+      children: [{
+        value: 'sin',
+        label: 'y = Sin(x)',
+        isLeaf: true
+      }, {
+        value: 'cos',
+        label: 'y = Cos(x)',
+        isLeaf: true
+      }, {
+        value: 'tan',
+        label: 'y = Tan(x)',
+        isLeaf: true
+      }
+      ]
     }, {
       value: 'zhejiang',
       label: 'Zhejiang',
@@ -74,40 +91,17 @@ export class FunctionCurveComponent implements OnInit {
     if (this.values.length <= 0) {
       return;
     }
-    const curval: string = this.values[0];
+    const curval: string = this.values[this.values.length - 1];
 
     switch (curval) {
       case 'abs':
         this.funcChartOption = of([]).pipe(
           map(() => {
             return {
-              title: {
-                text: 'y = abs(x)',
-                top: 'bottom',
-                left: 'center'
-              },
-              xAxis: {
-                type: 'value',
-                name: 'x',
-                interval: 1,
-                axisLine: {
-                  symbol: ['none', 'arrow'],
-                }
-              },
-              yAxis: {
-                type: 'value',
-                name: 'y',
-                min: -1,
-                interval: 1,
-                axisLine: {
-                  symbol: ['none', 'arrow']
-                }
-              },
-              series: [{
-                data: [[-5, 5], [-4, 4], [-3, 3], [-2, 2], [-1, 1], [0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5]],
-                type: 'line',
-                smooth: false,
-              }]
+              title: this.curveTitle('y = abs(x)'),
+              xAxis: this.curveXAxis(),
+              yAxis: this.curveYAxis(),
+              series: this.curveSeries(curval),
             };
           }));
         break;
@@ -116,42 +110,10 @@ export class FunctionCurveComponent implements OnInit {
         this.funcChartOption = of([]).pipe(
           map(() => {
             return {
-              title: {
-                text: 'y = sign(x)',
-                top: 'bottom',
-                left: 'center'
-              },
-              xAxis: {
-                type: 'value',
-                name: 'x',
-                interval: 1,
-                axisLine: {
-                  symbol: ['none', 'arrow'],
-                }
-              },
-              yAxis: {
-                type: 'value',
-                name: 'y',
-                interval: 1,
-                min: -2,
-                max: 2,
-                axisLine: {
-                  symbol: ['none', 'arrow']
-                }
-              },
-              series: [{
-                data: [[-5, -1], [-4, -1], [-3, -1], [-2, -1], [-1, -1], [0, -1]],
-                type: 'line',
-                smooth: false,
-              }, {
-                data: [[0, 0]],
-                type: 'line',
-                smooth: false,
-              }, {
-                data: [[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [4, 1]],
-                type: 'line',
-                smooth: false,
-              }]
+              title: this.curveTitle('y = sign(x)'),
+              xAxis: this.curveXAxis(),
+              yAxis: this.curveYAxis(),
+              series: this.curveSeries(curval),
             };
           }));
         break;
@@ -160,34 +122,10 @@ export class FunctionCurveComponent implements OnInit {
         this.funcChartOption = of([]).pipe(
           map(() => {
             return {
-              title: {
-                text: 'y = floor(x)',
-                top: 'bottom',
-                left: 'center'
-              },
-              xAxis: {
-                type: 'value',
-                name: 'x',
-                interval: 1,
-                axisLine: {
-                  symbol: ['none', 'arrow'],
-                }
-              },
-              yAxis: {
-                type: 'value',
-                name: 'y',
-                interval: 1,
-                min: -2,
-                max: 2,
-                axisLine: {
-                  symbol: ['none', 'arrow']
-                }
-              },
-              series: [{
-                data: [[-5, -1], [-4, -1], [-3, -1], [-2, -1], [-1, -1], [0, -1]],
-                type: 'line',
-                smooth: false,
-              }]
+              title: this.curveTitle('y = floor(x)'),
+              xAxis: this.curveXAxis(),
+              yAxis: this.curveYAxis(),
+              series: this.curveSeries(curval),
             };
           }));
         break;
@@ -196,42 +134,33 @@ export class FunctionCurveComponent implements OnInit {
         this.funcChartOption = of([]).pipe(
           map(() => {
             return {
-              title: {
-                text: 'y = sign(x)',
-                top: 'bottom',
-                left: 'center'
-              },
-              xAxis: {
-                type: 'value',
-                name: 'x',
-                interval: 1,
-                axisLine: {
-                  symbol: ['none', 'arrow'],
-                }
-              },
-              yAxis: {
-                type: 'value',
-                name: 'y',
-                interval: 1,
-                min: -2,
-                max: 2,
-                axisLine: {
-                  symbol: ['none', 'arrow']
-                }
-              },
-              series: [{
-                data: [[-5, -1], [-4, -1], [-3, -1], [-2, -1], [-1, -1], [0, -1]],
-                type: 'line',
-                smooth: false,
-              }, {
-                data: [[0, 0]],
-                type: 'line',
-                smooth: false,
-              }, {
-                data: [[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [4, 1]],
-                type: 'line',
-                smooth: false,
-              }]
+              title: this.curveTitle('y = sin(x)'),
+              xAxis: this.curveXAxis(),
+              yAxis: this.curveYAxis(),
+              series: this.curveSeries(curval),
+            };
+          }));
+        break;
+
+      case 'cos':
+        this.funcChartOption = of([]).pipe(
+          map(() => {
+            return {
+              title: this.curveTitle('y = cos(x)'),
+              xAxis: this.curveXAxis(),
+              yAxis: this.curveYAxis(),
+              series: this.curveSeries(curval),
+            };
+          }));
+        break;
+      case 'tan':
+        this.funcChartOption = of([]).pipe(
+          map(() => {
+            return {
+              title: this.curveTitle('y = tan(x)'),
+              xAxis: this.curveXAxis(),
+              yAxis: this.curveYAxis(),
+              series: this.curveSeries(curval),
             };
           }));
         break;
@@ -239,6 +168,136 @@ export class FunctionCurveComponent implements OnInit {
       default:
         break;
     }
+  }
 
+  private curveTitle(name: string): EChartTitleOption {
+    return {
+      text: name,
+      top: 'bottom',
+      left: 'center'
+    };
+  }
+  private curveXAxis(): EChartOption.XAxis {
+    return {
+      type: 'value',
+      name: 'x',
+      interval: 1,
+      axisLine: {
+        symbol: ['none', 'arrow'],
+      }
+    };
+  }
+  private curveYAxis(): EChartOption.YAxis {
+    return {
+      type: 'value',
+      name: 'y',
+      interval: 1,
+      // min: -2,
+      // max: 2,
+      axisLine: {
+        symbol: ['none', 'arrow']
+      }
+    };
+  }
+  private curveSeries(func: string): EChartOption.SeriesLine[] {
+    const data: any[] = [];
+    const series: EChartOption.SeriesLine[] = [];
+
+    switch (func) {
+      case 'abs':
+        for (let i = -5; i <= 5; i++) {
+          data.push([i, Math.abs(i)]);
+        }
+        series.push({
+          data,
+          type: 'line',
+          smooth: false,
+        });
+        break;
+
+      case 'sign':
+        for (let i = -5; i < 0; i++) {
+          data.push([i, Math.sign(i)]);
+        }
+        data.push([0, -1]);
+        series.push({
+          data,
+          type: 'line',
+          smooth: false,
+        });
+        data.splice(0);
+        data.push([0, 0]);
+        series.push({
+          data,
+          type: 'line',
+          smooth: false,
+        });
+        data.splice(0);
+        data.push([0, 1]);
+        for (let i = 1; i < 5; i++) {
+          data.push([i, Math.sign(i)]);
+        }
+        series.push({
+          data,
+          type: 'line',
+          smooth: false,
+        });
+        break;
+
+      case 'floor':
+        for (let i = -5; i <= 5; i++) {
+          data.push([i, Math.floor(i)]);
+        }
+        series.push({
+          data,
+          type: 'line',
+          smooth: false,
+        });
+        break;
+
+      case 'sin':
+        for (let i = -6.8; i < 6.8; i += 0.2) {
+          data.push([i, Math.sin(i)]);
+        }
+        series.push({
+          data,
+          type: 'line',
+          smooth: true,
+        });
+        break;
+
+      case 'cos':
+        for (let i = -6.8; i < 6.8; i += 0.2) {
+          data.push([i, Math.cos(i)]);
+        }
+        series.push({
+          data,
+          type: 'line',
+          smooth: true,
+        });
+        break;
+
+      case 'tan':
+        for (let i = 0.001 - (1 * Math.PI / 2); i <= 0.1 - (1 * Math.PI / 2); i += 0.05) {
+          data.push([i, Math.tan(i)]);
+        }
+        for (let i = 0.1 - (1 * Math.PI / 2); i <= (Math.PI / 2) - 0.1; i += 0.5) {
+          data.push([i, Math.tan(i)]);
+        }
+        for (let i = (Math.PI / 2) - 0.1; i < Math.PI / 2; i += 0.005) {
+          data.push([i, Math.tan(i)]);
+        }
+        series.push({
+          data,
+          type: 'line',
+          smooth: true,
+        });
+        break;
+
+      default:
+        break;
+    }
+
+    return series;
   }
 }
