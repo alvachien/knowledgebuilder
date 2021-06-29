27694811ce25b6957df68bf112b2759d7a42c88e
Alva Chien <alvachien@163.com>
2021-06-29 18:33:08 +0800

    Add new codes

--- src/app/models/award.ts
+++ src/app/models/award.ts
@@ -1,5 +1,104 @@
 /* eslint-disable no-underscore-dangle */
 
+/**
+ * Award Rule
+ */
+export enum AwardRuleTypeEnum {
+    GoToBedTime = 1,
+    SchoolWorkTime = 2,
+    HomeWorkCount = 3,
+    BodyExerciseCount = 4,
+    ErrorCollectionHabit = 5,
+    CleanDeakHabit = 6,
+    HouseKeepingCount = 7,
+    PoliteBehavior = 8,
+    HandWritingHabit = 9,
+}
+
+
+export class AwardRule {
+    ID: number;
+    RuleType: AwardRuleTypeEnum;
+    TargetUser: string;
+    Desp: string;
+    ValidFrom: moment.Moment;
+    ValidTo: moment.Moment;
+    CoutOfFactLow?: number;
+    CoutOfFactHigh?: number;
+    DoneOfFact?: boolean;
+    TimeStart?: number;
+    TimeEnd?: number;
+    DaysFrom?: number;
+    DaysTo?: number;
+    Point: number;
+
+    public IsValid(): boolean {
+        if (!this.TargetUser) {
+            return false;
+        }
+        if (!this.Desp) {
+            return false;
+        }
+
+        switch (RuleType) {
+            case AwardRuleTypeEnum.GoToBedTime:
+            case AwardRuleTypeEnum.SchoolWorkTime:
+                if (this.TimeStart === undefined || this.TimeEnd === undefined
+                    || this.TimeStart > this.TimeEnd)
+                    return false;
+                break;
+
+            case AwardRuleTypeEnum.HomeWorkCount:
+            case AwardRuleTypeEnum.BodyExerciseCount:
+            case AwardRuleTypeEnum.HouseKeepingCount:
+            case AwardRuleTypeEnum.PoliteBehavior:
+                if (this.CountOfFactLow === undefined)
+                    return false;
+                if (this.CountOfFactHigh === undefined)
+                    return false;
+                if (this.CountOfFactLow > this.CountOfFactHigh)
+                    return false;
+                break;
+
+            case AwardRuleTypeEnum.ErrorCollectionHabit:
+            case AwardRuleTypeEnum.CleanDeakHabit:
+                if (this.DoneOfFact === undefined)
+                    return false;
+                break;
+
+            default:
+                return false;
+        }
+
+        return true;
+    }
+}
+
+export class DailyTrace {
+    TargetUser: string;
+    RecordDate: moment.Moment;
+    SchoolWorkTime?: number;
+    GoToBedTime?: number;
+    HomeWorkCount?: number;
+    BodyExerciseCount?: number;
+    ErrorsCollection?: boolean;
+    HandWriting?: boolean;
+    CleanDesk?: boolean;
+    HouseKeepingCount?: number;
+    PoliteBehavior?: number;
+    Comment: string;
+}
+
+export class AwardPoint {
+    ID: number;
+    TargetUser: string;
+    RecordDate: moment.Moment;
+    MatchedRuleID?: number;
+    CountOfDay?: number;
+    Point: number;
+    Comment: string;
+}
+
 /***
  * Daily Behavior
  */
@@ -44,9 +143,9 @@ export class DailyBehavior {
 
 // eslint-disable-next-line no-shadow
 export enum RuleType {
-    goToBedTime             = 1,
-    schoolWorkTime          = 2,
-    additionalWorkCount     = 3,
+    goToBedTime = 1,
+    schoolWorkTime = 2,
+    additionalWorkCount = 3,
 }
 
 export interface AwardPoint {
@@ -75,8 +174,8 @@ export class DailyAwardRule {
     public ranges: ActivityTimeRange[] = [];
 
     private _ruleType: RuleType = RuleType.goToBedTime;
-    get ruleType(): RuleType    { return this._ruleType;    }
-    set ruleType(rt: RuleType)  { this._ruleType = rt;      }
+    get ruleType(): RuleType { return this._ruleType; }
+    set ruleType(rt: RuleType) { this._ruleType = rt; }
 }
 
 export class OneTimeAwardRule {
