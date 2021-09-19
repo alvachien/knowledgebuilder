97a4449c5092a750b0b7ad33fbb934864efe10ab
Alva Chien <alvachien@live.com>
2021-09-20 00:31:14 +0800

    New codes

--- src/app/pages/exercise-items/exercise-items-routing.module.ts
+++ src/app/pages/exercise-items/exercise-items-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ExerciseItemsComponent } from './exercise-items.component';
 import { ExerciseItemDetailComponent } from './exercise-item-detail';
 import { ExerciseItemSearchComponent } from './exercise-item-search';
+import { ExerciseItemScoreComponent } from './exercise-item-score';
 
 const routes: Routes = [
   { path: '', component: ExerciseItemsComponent },
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'display/:id', component: ExerciseItemDetailComponent },
   { path: 'edit/:id', component: ExerciseItemDetailComponent },
   { path: 'search', component: ExerciseItemSearchComponent },
+  { path: 'score', component: ExerciseItemSearchComponent },
 ];
 
 @NgModule({
