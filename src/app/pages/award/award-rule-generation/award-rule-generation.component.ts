374d297c8479a4c20a5b61499719ab60931a1825
Alva Chien <alvachien@live.com>
2021-10-03 00:48:47 +0800

    Add new codes

--- src/app/pages/award/award-rule-generation/award-rule-generation.component.ts
+++ src/app/pages/award/award-rule-generation/award-rule-generation.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import moment from 'moment';
@@ -6,8 +7,8 @@ import { AwardRule, AwardRuleTypeEnum, getAwardRuleTypeNames } from 'src/app/mod
 import { ODataService } from 'src/app/services';
 
 class DimensionInfo {
-  from: number = 0;
-  to: number = 0;
+  from = 0;
+  to = 0;
 
   public toString(): string {
     return `[${this.from} - ${this.to})`;
@@ -15,10 +16,27 @@ class DimensionInfo {
 }
 
 class ContinuedDaysInfo {
-  from: number = 0;
-  to: number = 0;
+  from = 0;
+  to = 0;
   public toString(): string {
     return `[${this.from} - ${this.to})`;
+  }
+}
+
+class PointInfo {
+  dimInfo: DimensionInfo;
+  // condInfo: ContinuedDaysInfo;
+  points: {index: number; point: number}[] = [];
+  isEdit = false;
+
+  public getPoints(col: string): number {
+    if (col && col.startsWith('days')) {
+      return +col.replace('days', '');
+    }
+    return -1;
+  }
+  constructor(dim: DimensionInfo, ) {
+    this.dimInfo = dim;
   }
 }
 
@@ -28,40 +46,39 @@ class ContinuedDaysInfo {
   styleUrls: ['./award-rule-generation.component.scss']
 })
 export class AwardRuleGenerationComponent implements OnInit {
-  // eslint-disable-next-line @angular-eslint/component-class-suffix
-  displayedColumns: string[] = ['dimension', 'point', 'edit'];
-  dataSource: AwardRule[] = [];
+  // Step 1: HEADER
+  firstFormGroup: FormGroup;
+  arRuleTypes: any[] = [];
+  // Step 2: DIEMENSIONS
+  secondFormGroup: FormGroup;
+  dimensions: DimensionInfo[] = [];
+  // Step 3: DAYS
+  thirdFormGroup: FormGroup;
+  contDays: ContinuedDaysInfo[] = [];
+  // Step 4: POINTS
+  displayedColumns: string[] = ['dimension'];
   dataSchema: { [key: string]: any } = {
     point: 'number',
     edit: 'edit',
   };
-  step = 0;
-  arRuleTypes: any[] = [];
-  selectedRuleType: AwardRuleTypeEnum = AwardRuleTypeEnum.GoToBedTime;
-  targetUser = '';
-  desp = '';
-  validFrom: moment.Moment = moment();
-  validTo: moment.Moment = moment();
-  rawDimension = '';
-  rawContDay = '';
-  dimensions: DimensionInfo[] = [];
-  contDays: ContinuedDaysInfo[] = [];
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  thirdFormGroup: FormGroup;
+  dataSource: PointInfo[] = [];
+  // Step 5.
 
   constructor(private _formBuilder: FormBuilder,
     private odataSrv: ODataService) {
     this.arRuleTypes = getAwardRuleTypeNames();
 
     this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
+      targetuserCtrl: ['', Validators.required],
+      validFromCtrl: [undefined, Validators.required],
+      validToCtrl: [undefined, Validators.required],
+      ruleTypeCtrl: [AwardRuleTypeEnum.GoToBedTime, Validators.required]
     });
     this.secondFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
+      rawCtrl: ['', Validators.required]
     });
     this.thirdFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
+      rawCtrl: ['', Validators.required]
     });
   }
 
@@ -70,31 +87,37 @@ export class AwardRuleGenerationComponent implements OnInit {
   }
 
   public ngOnInit(): void {
-    // Todo.
+    this.firstFormGroup.get('validFromCtrl')?.setValue(moment());
+    this.firstFormGroup.get('validToCtrl')?.setValue(moment());
+
+    this.secondFormGroup.get('rawCtrl')?.valueChanges.subscribe(val => {
+      this.onDimensionChange(val);
+    });
+    this.thirdFormGroup.get('rawCtrl')?.valueChanges.subscribe(val => {
+      this.onContDayChange(val);
+    });
   }
 
-  setStep(index: number) {
-    this.step = index;
-  }
-  nextStep() {
-    this.step++;
-  }
-  prevStep() {
-    this.step--;
+  get selectedRuleType(): AwardRuleTypeEnum {
+    const rtval = this.firstFormGroup.get('ruleTypeCtrl')?.value;
+    if (rtval !== undefined) {
+      return rtval as AwardRuleTypeEnum;
+    }
+    return AwardRuleTypeEnum.GoToBedTime;
   }
 
-  public onDimensionChange(event: any) {
+  public onDimensionChange(val: any) {
     this.dimensions = [];
 
-    if (this.rawDimension) {
-      const subd = this.rawDimension.split(';');
+    if (val) {
+      const subd = val.split(';');
       switch(this.selectedRuleType) {
         case AwardRuleTypeEnum.BodyExerciseCount:
           break;
 
         case AwardRuleTypeEnum.GoToBedTime:
         default: {
-          let submap = subd.map(v => {
+          let submap = subd.map((v: string | number) => {
             if (v) {
               const tp = +v;
               if (!isNaN(tp)) {
@@ -105,17 +128,17 @@ export class AwardRuleGenerationComponent implements OnInit {
             }
             return 0;
           });
-          submap = submap.filter(v => v > 0);
+          submap = submap.filter((v: number) => v > 0);
           submap.sort();
           let tpcur = 0;
-          submap.forEach(sd => { 
-            let di = new DimensionInfo();
+          submap.forEach((sd: number) => {
+            const di = new DimensionInfo();
             di.from = tpcur;
             di.to = sd;
             this.dimensions.push(di);
             tpcur = sd;
           });
-          let di2 = new DimensionInfo();
+          const di2 = new DimensionInfo();
           di2.from = tpcur;
           di2.to = 24;
           this.dimensions.push(di2);
@@ -124,15 +147,15 @@ export class AwardRuleGenerationComponent implements OnInit {
       }
     }
   }
-  public onContDayChange(event: any) {
+  public onContDayChange(val: any) {
     this.contDays = [];
 
-    if (this.rawContDay) {
-      const subd = this.rawContDay.split(';');
-      let submap = subd.map(v => {
+    if (val) {
+      const subd = val.split(';');
+      let submap = subd.map((v: string | number) => {
         if (v) {
           const tp = +v;
-          if (!isNaN(tp)) {            
+          if (!isNaN(tp)) {
             if (tp > 0) {
               return tp;
             }
@@ -140,20 +163,48 @@ export class AwardRuleGenerationComponent implements OnInit {
         }
         return 0;
       });
-      submap = submap.filter(v => v > 0);
+      submap = submap.filter((v: number) => v > 0);
       submap.sort();
       let daycur = 0;
-      submap.forEach(sd => { 
-        let di = new ContinuedDaysInfo();
+      submap.forEach((sd: number) => {
+        const di = new ContinuedDaysInfo();
         di.from = daycur;
         di.to = sd;
         this.contDays.push(di);
         daycur = sd;
       });
-      let di2 = new ContinuedDaysInfo();
+      const di2 = new ContinuedDaysInfo();
       di2.from = daycur;
       di2.to = Number.POSITIVE_INFINITY;
       this.contDays.push(di2);
     }
+  }
+  public onInitialRules(): void {
+    this.dataSource = [];
+    this.displayedColumns = ['dimension'];
+
+    // Columns
+    for(let i = 0; i < this.contDays.length; i ++) {
+      this.displayedColumns.push(`days${i}`);
+    }
+
+    // Initialize data
+    this.dimensions.forEach(dim => {
+      const pi = new PointInfo(dim);
+      for(let i = 0; i < this.contDays.length; i ++) {
+        pi.points.push({
+          index: i,
+          point: 0
+        });
+      }
+      this.dataSource.push(pi);
+    });
+  }
+  public getColumnHeaderForDays(col: string): string {
+    if (col && col.startsWith('days')) {
+      const idx = +col.replace('days', '');
+      return this.contDays[idx].toString();
+    }
+    return '';
   }
 }
