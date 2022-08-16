6be19a47563cff8c862c513e905fe17c17673847
Alva Chien <alvachien@163.com>
2022-08-16 21:17:38 +0800

    Bump version to 0.3.135

--- src/environments/environment.github.ts
+++ src/environments/environment.github.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '0.3.132',
+  version: '0.3.135',
   mockdata: true,
   basehref: '/knowledgebuilder/',
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   mockdata: false,
-  version: '0.3.132',
+  version: '0.3.135',
   basehref: '/',
 
   idServerUrl: 'https://localhost:44353/',
