58f378156645913591268329340f57437b28e665
Alva Chien <alvachien@163.com>
2022-11-26 15:34:36 +0800

    Update auth lib

--- src/app/auth/auth-config.module.ts
+++ src/app/auth/auth-config.module.ts
@@ -17,15 +17,15 @@ import { environment } from 'src/environments/environment';
             silentRenew: true,
             useRefreshToken: true,
             // silentRenewUrl: window.location.origin + '/silent-renew.html',
-            renewTimeBeforeTokenExpiresInSeconds: 666,
-            tokenRefreshInSeconds: 600,
+            // renewTimeBeforeTokenExpiresInSeconds: 666,
+            // tokenRefreshInSeconds: 600,
 
-            disableIdTokenValidation: true,
+            // disableIdTokenValidation: true,
             //ignoreNonceAfterRefresh: true, // this is required if the id_token is not returned
             // allowUnsafeReuseRefreshToken: true, // this is required if the refresh token is not rotated
             //triggerRefreshWhenIdTokenExpired: false, // required to refresh the browser if id_token is not updated after the first authentication
             //autoUserInfo: false, // if the user endpoint is not supported
-            logLevel: LogLevel.Debug,
+            logLevel: LogLevel.Error,
         }
       })],
     exports: [AuthModule],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '0.3.146',
+  version: '0.3.150',
   mockdata: false,
   basehref: '/math/',
 
